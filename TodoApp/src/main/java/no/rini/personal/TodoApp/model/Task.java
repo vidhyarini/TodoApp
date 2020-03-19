ffa4e8cb6391241d7eceace66ecdef94e8033857
Vidhya Rini <rini.vidhya88@gmail.com>
2020-03-19 16:05:09 +0100

    Task added (#2)

--- TodoApp/src/main/java/no/rini/personal/TodoApp/model/Task.java
+++ TodoApp/src/main/java/no/rini/personal/TodoApp/model/Task.java
@@ -1,4 +1,37 @@
 package no.rini.personal.TodoApp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Task {
+
+    @NotNull
+    @Id
+
+    private Integer id;
+    private String Studentname;
+    private String Remarks;
+
+    public Task() {
+    }
+
+    public Task(@NotNull Integer id,String studentname,String remarks){
+        this.id = id;
+        this.Studentname = studentname;
+        this.Remarks = remarks;
+    }
+
+    public Integer getId() {return id;}
+
+    public void setId(Integer id) {this.id = id;}
+
+    public String getStudentname() {return Studentname;}
+
+    public void setStudentname(String studentname) {Studentname = studentname;}
+
+    public String getRemarks() {return Remarks;}
+
+    public void setRemarks(String remarks) {Remarks = remarks;}
 }
